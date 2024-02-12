79e2582342b16aa1b2cc56222e6192de1c11b8e5
Nikita Shaposhnik <nikita.shaposhnik@gmail.com>
2024-02-13 02:28:32 +0300

    change paths and imports

--- MLOps/infer.py
+++ MLOps/infer.py
@@ -2,20 +2,19 @@ import pickle
 
 import numpy as np
 import pandas as pd
-
-from .common import dataset_to_X_y
+from common import dataset_to_X_y
 
 
 def test():
     print("Loading dataset...")
-    X, y = dataset_to_X_y("data/test.csv")
+    X, y = dataset_to_X_y("../data/test.csv")
     print("Dataset loaded")
-    model = pickle.load(open("data/random_forest.pth", "rb"))
+    model = pickle.load(open("../data/random_forest.pth", "rb"))
     y_predicted = model.predict(X)
     mistake_rate = 1 - np.sum(np.abs(y - y_predicted)) / len(y)
     print(f"Accuracy: {mistake_rate.round(2)}")
     df = pd.DataFrame(y_predicted)
-    df.to_csv("data/result.csv", header=False, index=False)
+    df.to_csv("../data/result.csv", header=False, index=False)
 
 
 if __name__ == "__main__":
--- MLOps/train.py
+++ MLOps/train.py
@@ -1,19 +1,18 @@
 import pickle
 
+from common import dataset_to_X_y
 from sklearn.ensemble import RandomForestClassifier
-
-from .common import dataset_to_X_y
 
 
 def train():
     print("Loading dataset...")
-    X, y = dataset_to_X_y("data/train.csv")
+    X, y = dataset_to_X_y("../data/train.csv")
     print("Dataset loaded")
     model = RandomForestClassifier(max_depth=2, random_state=0)
     print("Start training...")
     model.fit(X, y)
     print("Training done")
-    pickle.dump(model, open("data/random_forest.pth", "wb"))
+    pickle.dump(model, open("../data/random_forest.pth", "wb"))
 
 
 if __name__ == "__main__":
