5bc90808c5a0052a3526e51439d645936d481727
Nikita Shaposhnik <nikita.shaposhnik@gmail.com>
2024-03-04 18:13:00 +0300

    change order

--- MLOps/train.py
+++ MLOps/train.py
@@ -6,7 +6,6 @@ from sklearn.ensemble import RandomForestClassifier
 
 def train():
     common.load_data()
-    return
     X, y = common.dataset_to_X_y("diabetes/train.csv")
     model = RandomForestClassifier(max_depth=2, random_state=0)
     print("Start training...")
