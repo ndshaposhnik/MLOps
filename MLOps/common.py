097a3d0b55de6152430e81a381d6ecdfd8297603
Nikita Shaposhnik <nikita.shaposhnik@gmail.com>
2024-03-04 18:20:50 +0300

    finally configure dvc

--- MLOps/common.py
+++ MLOps/common.py
@@ -24,7 +24,7 @@ def load_data():
     print("Loading dataset...")
     fs = DVCFileSystem(
         url="https://github.com/ndshaposhnik/MLOps.git",
-        rev="task1",
+        rev="master",
     )
     fs.get("./data", "./diabetes", recursive=True)
     print("Dataset loaded")
